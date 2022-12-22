0f09e1bffb4e5ead9f1fc7375c117888039af0b7
PRESTIGE <mamatqulov@gmail.com>
2022-12-22 20:47:21 +0500

    first

--- basic_func11.py
+++ basic_func11.py
@@ -1,10 +1,15 @@
 def main(a):
     '''find the absolute value of a and return it
-    
+
     Args:
         a (int): a number
-        
+
     Returns:
         int: the absolute value.
     '''
-    return
+    return abs(a)
+    number = int(a)
+    return abs(number)
+
+
+print(main(3.4))
