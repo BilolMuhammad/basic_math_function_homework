f2b334f8623be9605fa03bc8a64d998abd3d41c7
PRESTIGE <mamatqulov@gmail.com>
2022-12-24 18:36:39 +0500

    13

--- basic_func13.py
+++ basic_func13.py
@@ -1,10 +1,17 @@
+import math
+
+
 def main(a):
     '''find the square root of a number and return it.
-    
+
     Args:
         a (int): a number
-        
+
     Returns:
         float: the absolute value.
     '''
-    return
+    number = int(a)
+    return math.sqrt(abs(number))
+
+
+print(main(-196))
