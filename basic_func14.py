0079cca359563b1ec095854a2107736bfd122b21
PRESTIGE <mamatqulov@gmail.com>
2022-12-24 18:37:10 +0500

    14

--- basic_func14.py
+++ basic_func14.py
@@ -1,11 +1,17 @@
+import math
+
+
 def main(a, b):
     '''find the floor division of a and b and return it.
-    
+
     Args:
         a (int): a number
         b (int): a number
-        
+
     Returns:
         int: the result.
     '''
-    return 
+    return math.floor(a), math.floor(b)
+
+
+print(main(3.4, 5.5))
