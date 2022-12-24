01f9c41f3e5077cd1251597693dcac43df431ca0
PRESTIGE <mamatqulov@gmail.com>
2022-12-24 18:38:06 +0500

    16

--- basic_func16.py
+++ basic_func16.py
@@ -1,11 +1,14 @@
 def main(a, b):
     '''find the sum of a and b and return it.
-    
+
     Args:
         a (int): a number
         b (int): a number
-        
+
     Returns:
         int: the sum.
     '''
-    return
+    return int(a) + int(b)
+
+
+print(main(4, 2))
