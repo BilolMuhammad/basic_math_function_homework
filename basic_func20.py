b9e0f0fb030e35367a02b4a485ca934a8b832b80
PRESTIGE <mamatqulov@gmail.com>
2022-12-24 18:40:56 +0500

    20

--- basic_func20.py
+++ basic_func20.py
@@ -1,11 +1,14 @@
 def main(a, b):
     '''find the multiplication of a and b and return it.
-    
+
     Args:
         a (int): a number
         b (int): a number
-        
+
     Returns:
         int: the result.
     '''
-    return
+    return int(a) * int(b)
+
+
+print(main(4, 5))
